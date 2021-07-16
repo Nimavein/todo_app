ab36fca979a0b71fe3a0d2d9aee3fe69c2c14b17
Nimavein <michalzygiel00@gmail.com>
2021-07-16 09:06:09 +0200

    todos container styling add,backdrop on scroll fixed, sticky navbar on scroll

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,15 @@
 import { useGlobalContext } from "./context";
 import React, { useState } from "react";
-import Task from "./Task";
 import axios from "axios";
 import TodoEditForm from "./TodoEditForm";
+import { FaTrash } from "react-icons/fa";
 
-const Todo = ({ id, name, task }) => {
+const Todo = ({ id, name, task, created_at }) => {
   const { todoList, setTodoList, jwt } = useGlobalContext();
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const whenCreated = created_at.slice(0, 10);
+  const allTasksNumber = task.length;
+  const doneTasksNumber = task.filter((item) => item.isDone === true).length;
 
   const deleteTodo = (id) => {
     const newTodos = todoList.filter((todo) => todo.id !== id);
@@ -23,11 +26,19 @@ const Todo = ({ id, name, task }) => {
   };
 
   return (
-    <div>
-      <h1>{name}</h1>
-
-      <button onClick={(e) => deleteTodo(id)}>delete</button>
-      <button onClick={() => handleEditVisibility()}>edit</button>
+    <div onClick={() => handleEditVisibility()} className="todo">
+      <p className="todo-name">{name}</p>
+      <p className="todo-create-date">{`Created at: ${whenCreated}`}</p>
+      <div className="tasks-numbers">
+        <p className="completed-tasks-text">{`Completed: ${doneTasksNumber}`}</p>
+        <p className="uncompleted-tasks-text">{`Uncompleted: ${
+          allTasksNumber - doneTasksNumber
+        }`}</p>
+        <p className="all-tasks-text">{`All: ${allTasksNumber}`}</p>
+      </div>
+      <button className="todo-delete-button" onClick={(e) => deleteTodo(id)}>
+        <FaTrash className="delete-icon" />
+      </button>
       {isEditOpen && (
         <TodoEditForm
           id={id}
